091d4dc0b4fb197652f2578cd9ec133f1a4c726d
XzetsX <91646606+XzetsX@users.noreply.github.com>
2022-04-13 18:50:10 +0300

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -4,4 +4,4 @@ import requests
 url = input("enter vid link:\n")
 r = requests.get(url, allow_redirects=True)
 
-open('facebook.ico', 'wb').write(r.content)
+open('download.mp4', 'wb').write(r.content)
